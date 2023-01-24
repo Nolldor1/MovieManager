7201c30525ddb9b3dbd59a40fa0816918eaf268a
German Gizatullin <snolldors@inbox.ru>
2023-01-24 23:51:27 +0500

    add MovieManager and Tests and update pom

---
+++ src/main/java/ru/netology/MovieManager.java
@@ -0,0 +1,43 @@
+package ru.netology;
+
+public class MovieManager {
+    private String[] films = new String[0];
+    private int limit;
+
+    public MovieManager() {
+        this.limit = 10;
+    }
+
+    public MovieManager(int limit) {
+        this.limit = limit;
+    }
+
+    public void addFilm(String film) {
+        String[] tmp = new String[films.length + 1];
+        for (int i = 0; i < films.length; i++) {
+            tmp[i] = films[i];
+        }
+        tmp[tmp.length - 1] = film;
+        films = tmp;
+    }
+
+    public String[] findAll() {
+        return films;
+    }
+
+    public String[] findLast(int limit) {
+        int resultlenght;
+        if (films.length < limit) {
+            resultlenght = films.length;
+        } else {
+            resultlenght = limit;
+        }
+        String[] tmp = new String[resultlenght];
+        for (int i = 0; i < tmp.length; i++) {
+            tmp[i] = films[films.length - 1 - i];
+        }
+        return tmp;
+    }
+
+}
+
